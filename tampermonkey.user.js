2b6c61eb53859353abd6a3567710076a6905da87
Duc Tien? <altugphone5@gmail.com>
2025-10-17 15:14:44 +0700

    Create tampermonkey.user.js

---
+++ tampermonkey.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name        DucTien Alpha 4.0
+// @namespace   http://tampermonkey.net/
+// @version     4.0
+// @description To solve exercises
+// @author      DucTien
+// @require     https://raw.githubusercontent.com/ductienalpha/DucTienAlpha4.0/refs/heads/main/ductien-alpha.js
+// @match       https://*/*
+// @icon        https://i.pinimg.com/1200x/0d/fc/a6/0dfca60200be23af53d552ead0d535be.jpg
+// @grant       GM_addStyle
+// @grant       GM_xmlhttpRequest
+// @grant       GM.xmlHttpRequest
+// @grant       GM.getValue
+// @grant       GM.setValue
+// @grant       GM.deleteValue
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_deleteValue
+// ==/UserScript==
